902c393a0bdf3fc390d4112970b604c008cb462c
jfouk <fouk2@illinois.edu>
2016-01-03 00:06:29 -0600

    Add daily script to create Journal Entry and symlink TODAY.org
    - Also need to create $ORG_NOTES variable in bashrc

---
+++ scripts/daily_orgnotes_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to create daily org file for each day
+# Also links a TODAY.org each day for easier access
+
+# check if orgnotes path exists
+if [ -z "$ORG_NOTES" ]; then
+    echo "No org_notes path is set! Set ORG_NOTES. "
+    exit 1
+fi
+
+# create new org file for the day
+touch $ORG_NOTES/Journal/$(date +%Y)/$(date +%A_%b_%d).org
+echo $(date +%A_%b_%d)".org created! "
+
+# update TODAY symlink
+rm $ORG_NOTES/Journal/TODAY.org
+ln -s $ORG_NOTES/Journal/$(date +%Y)/$(date +%A_%b_%d).org $ORG_NOTES/Journal/TODAY.org
+echo "TODAY.org symlink created! "
+
